e3f4f07432fc6b33012b755ef242cb754a46c2f0
cfergpro <colby.ferguson.pro@gmail.com>
2026-09-17 10:51:49 -0400

    Assignment 2
    Added main method and initialized rocket IDs and grades array.

--- GradeMatrixAnalyzer.java
+++ GradeMatrixAnalyzer.java
@@ -1,3 +1,27 @@
 public class GradeMatrixAnalyzer {
+    public static void main(String[] args) {
+        int[] rocketids = {1001, 1004, 1002, 1005, 1003};
+        double[][] grades = {{88.5, 92.0, 79.0, 85.5},
+                            {}};
+    }
 
+    public static double[] computeStudentAverages(double[][] matrix) {
+        return;
+    }
+
+    public static double[] computeExamAverages(double[][] matrix) {
+        return;
+    }
+
+    public static int searchRocketID(int[] ids, int targetId) {
+        return;
+    }
+
+    public static double[] selectionSortCopy(double[] list) {
+        return;
+    }
+
+    public static int binarySearchScore(double[] sortedList, double target) {
+        return;
+    }
 }
